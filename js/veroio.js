933bfa940c989a4dbefd6f6ef6b35dc4b9fa0813
VeroIO <ITITIU14056@student.hcmiu.edu.vn>
2018-11-26 23:45:26 +0700

    Site updated: 2018-11-26 23:45:26

--- js/veroio.js
+++ js/veroio.js
@@ -10,29 +10,28 @@ $(document).ready(function () {
         }
     }
     share();
-})
-$(function () {
-    $.ajax({
-        url: "../music.json",
-        success: function (e) {
-            const ap = new APlayer({
-                container: document.getElementById('aplayer'),
-                mini: true,
-                fixed: true,
-                autoplay: false,
-                loop: 'all',
-                order: 'random',
-                preload: 'auto',
-                volume: 0.7,
-                mutex: true,
-                listFolded: false,
-                listMaxHeight: '320px',
-                lrcType: 3,
-                music: e
-            });
-        }
-    })
-})
-$(document).ready(function () {
+    function aplayerInitialize() {
+        $.ajax({
+            url: "../music.json",
+            success: function (e) {
+                const ap = new APlayer({
+                    container: document.getElementById("aplayer"),
+                    mini: true,
+                    fixed: true,
+                    autoplay: false,
+                    loop: "all",
+                    order: "random",
+                    preload: "auto",
+                    volume: 0.7,
+                    mutex: true,
+                    listFolded: false,
+                    listMaxHeight: "320px",
+                    lrcType: 3,
+                    music: e
+                });
+            }
+        });
+    }
+    aplayerInitialize();
     $(".fancybox").fancybox();
-});
+})
