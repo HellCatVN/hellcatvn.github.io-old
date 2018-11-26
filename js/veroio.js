0089e17ecc0e42d8e2a97966499bb9480b5e6dab
VeroIO <ITITIU14056@student.hcmiu.edu.vn>
2018-11-26 23:39:04 +0700

    Site updated: 2018-11-26 23:39:02

---
+++ js/veroio.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    function share() {
+        if ($(".post-share").length) {
+            $(".post-share").share({
+                disabled: ['douban', 'diandian'],
+                wechatQrcodeTitle: "WeChat Scan",
+                wechatQrcodeHelper: "<p>WeChat scan, share in the upper right corner</p>",
+                source: "HellCatVN Blog"
+            });
+        }
+    }
+    share();
+})
+$(function () {
+    $.ajax({
+        url: "../music.json",
+        success: function (e) {
+            const ap = new APlayer({
+                container: document.getElementById('aplayer'),
+                mini: true,
+                fixed: true,
+                autoplay: false,
+                loop: 'all',
+                order: 'random',
+                preload: 'auto',
+                volume: 0.7,
+                mutex: true,
+                listFolded: false,
+                listMaxHeight: '320px',
+                lrcType: 3,
+                music: e
+            });
+        }
+    })
+})
+$(document).ready(function () {
+    $(".fancybox").fancybox();
+});
